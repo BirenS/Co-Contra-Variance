9267f251fa1e84595723b16e7de31b1d89e554ab
BirenS <soni.biren@gmail.com>
2016-08-23 13:42:38 +0530

    Tested contravariant generic interface

---
+++ Co-Contra-Variance/Co-Contra-Variance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Co_Contra_Variance
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var animal = new GStack<Animal>();
+            IPushable<Animal> pushAnimal = animal;
+            IPushable<Bear> pushBear = pushAnimal;
+
+            pushBear.Push(new Bear("B1"));
+            pushBear.Push(new Bear("B2"));
+
+            IPoppable<Animal> popAnimal = animal;
+            for (int i = 0; i <= animal.Count; i++)
+                Console.WriteLine(popAnimal.Pop().Name.ToString());
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
