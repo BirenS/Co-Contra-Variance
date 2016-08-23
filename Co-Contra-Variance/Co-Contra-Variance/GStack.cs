6ee58e0f461b77393ca0cac557e8cd4b66269f82
ce001 <asan2011ster@gmail.com>
2016-08-23 13:13:05 +0530

    Implented IPoppable and IPushable interfaces through generic GStack class.

---
+++ Co-Contra-Variance/Co-Contra-Variance/GStack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Co_Contra_Variance
+{
+   public class GStack<T>: IPoppable<T>,IPushable<T>
+    {
+       int position;
+       public int Count { get; set; }
+       T[] data = new T[10];
+       public void Push(T a)
+       {
+           data[position++] = a;
+           Count++;
+       }
+       public T Pop()
+       {
+           Count--;
+           return data[--position];
+       }    
+
+    }
+}
