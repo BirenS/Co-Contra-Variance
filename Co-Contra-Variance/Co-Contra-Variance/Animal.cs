31ccbad54037329bd7d3639e663d6ae3e68f3874
ce001 <asan2011ster@gmail.com>
2016-08-23 13:03:18 +0530

    Created Animal as a public Base class which contains Two overloaded constructors and one Automatic public Property

---
+++ Co-Contra-Variance/Co-Contra-Variance/Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Co_Contra_Variance
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public Animal() { }
+        public Animal(String a)
+        {
+            this.Name = a;
+        }
+    }
+}
