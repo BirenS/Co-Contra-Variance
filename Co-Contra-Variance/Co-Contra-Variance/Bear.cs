ffade646e627a7b19b6fa3f2c3b1f724e574202c
ce001 <asan2011ster@gmail.com>
2016-08-23 13:05:15 +0530

    Implemented Bear as a child class which inherits from Animal class.

---
+++ Co-Contra-Variance/Co-Contra-Variance/Bear.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Co_Contra_Variance
+{
+    public class Bear: Animal
+    {
+        public Bear() { }
+        public Bear(string s)
+        {
+            this.Name = s;
+        }
+
+    }
+}
